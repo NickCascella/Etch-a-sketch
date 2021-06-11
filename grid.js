2de1d9329980c2a5f55d744491aebf6685da763f
WhatAmIDoingWrong98 <83091530+WhatAmIDoingWrong98@users.noreply.github.com>
2021-06-11 14:00:03 -0400

    Add files via upload

--- grid.js
+++ grid.js
@@ -3,31 +3,35 @@
 
 function restart() {
     let check = prompt('please enter grid size')
-    console.log('check me in console')
-    let i = 0
-    let wh = 1/check * 100 + '%'
-    let ht = 1/check * 100 + '%'
-    for(i=0; i<check*check; ++i){
-    let color = () => {
-        grid.style.backgroundColor = `rgb(${Math.random()*256}, ${Math.random()*256}, ${Math.random()*256})`
-    }
-    let clear = () => {
-        //grid.style.backgroundColor = `rgb(256, 256, 256)` 
-        grid.remove()
-        //let check = prompt('please enter grid size')
+    if(check < 100) {
+        let i = 0
+        let wh = 1/check * 100 + '%'
+        let ht = 1/check * 100 + '%'
+        parent.innerHTML = ''
+            for(i=0; i<check*check; ++i){
+            let color = () => {
+                grid.style.backgroundColor = `rgb(${Math.random()*256}, ${Math.random()*256}, ${Math.random()*256})`
+            }
+            let grid = document.createElement('div')
+            grid.style,minWidth = wh 
+            grid.style.width = wh
+            grid.style.height = ht
+            grid.className = 'test'
+            //grid.textContent = `${i + 1}`
+            grid.addEventListener('mouseover', color)
+            parent.appendChild(grid)
+            }
+
+    } else {
+        alert('please enter a number less than 100')
         restart()
-        
-    }
-    let grid = document.createElement('div')
-    grid.style,minWidth = wh 
-    grid.style.width = wh
-    grid.style.height = ht
-    grid.className = 'test'
-    //grid.textContent = `${i + 1}`
-    grid.addEventListener('mouseover', color)
-    parent.appendChild(grid)
-    btn.addEventListener('click', clear)
-    
+    }   
 }
-}
+
 restart()
+
+let clear = () => {
+    restart()
+}
+
+btn.addEventListener('click', clear)
